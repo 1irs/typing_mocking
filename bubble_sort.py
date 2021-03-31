f3ac6b30f752c6485ae27b25f9d7d031fed4e472
Vladimir Obrizan <volodymyr.obrizan@gmail.com>
2021-03-31 19:55:02 +0300

    Revert "Умышленная ошибка, чтобы проверить пайплайн"
    This reverts commit 8cbea65527fbc04bd64ee55bca6b8c39230510f0.

--- bubble_sort.py
+++ bubble_sort.py
@@ -10,6 +10,6 @@ def bubble_sort(seq):
         changed = False
         for i in range(len(seq) - 1):
             if seq[i] > seq[i + 1]:
-                seq[i], seq[i + 1] = seq[i + 1], seq[i + 1]
+                seq[i], seq[i + 1] = seq[i + 1], seq[i]
                 changed = True
     return seq
